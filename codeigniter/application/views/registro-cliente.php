60184da4e95a4ac787eb18723d8bc62664b009b5
Priscila Esquer <priscilaesquer@Priscilas-MBP.localdomain>
2019-06-01 12:55:33 -0700

    Cambios de estilo

---
+++ codeigniter/application/views/registro-cliente.php
@@ -0,0 +1,13 @@
+<div class="container">
+<p class="big-titles"> Registrar cliente </p>
+<button class="boton-azul"> Registrar clientes </button>
+
+<p class="texto-general-azul"> hola </p>
+<p class="texto-general-gris"> hola </p>
+<p class="texto-general-verde"> hola </p>
+<p class="texto-general-lightgray"> hola </p>
+<p class="texto-general-gris-bold"> hola </p>
+
+
+
+</div>
