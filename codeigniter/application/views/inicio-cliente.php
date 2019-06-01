798e37e34a15b62de5ec2a752254ac4c496b1b26
Diana Laura <dianamartinez@MBP-de-Laura.localdomain>
2019-06-01 12:57:20 -0700

    inicio cliente

---
+++ codeigniter/application/views/inicio-cliente.php
@@ -0,0 +1,38 @@
+<h1>Bienvenida, Laura <h1>
+
+<section id="login">
+    <div class="container-fluid">
+        <div class="row">
+            <section id="info-cliente">
+                <article class="col-lg-5">
+
+                  <div class ="perfil">
+                    <img class="foto-perfil" src="/imgs/login-foto.png">
+                    <p>Diana Laura Martínez García</p>
+                    <button>Editar perfil</button>
+                  </div>
+                  <div class = "caja perfil">
+                    <p> Marca </p>
+                    <p class = "info"> Audi</p>
+
+                    <p> Marca </p>
+                    <p class = "info"> Audi</p>
+                  </div>
+                </article>
+            </section>
+            <section class="mensajes">
+                <article class="col-lg-7">
+                    <p> nada <p>
+                </article>
+            </section>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+</body>
+</html>
+
