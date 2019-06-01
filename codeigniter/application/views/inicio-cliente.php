625cc1b7effa4a7585f9ccb7d4d95041751735ae
Diana Laura <dianamartinez@MBP-de-Laura.localdomain>
2019-06-01 16:54:59 -0700

    inicio cliente

--- codeigniter/application/views/inicio-cliente.php
+++ codeigniter/application/views/inicio-cliente.php
@@ -1,37 +1,77 @@
-<h1>Bienvenida, Laura <h1>
+<section id="dashboard">
+    <div class="container">
 
-<section id="login">
-    <div class="container-fluid">
         <div class="row">
+          <div class = "push-left">
+            <p class = "big-titles">Bienvenida, Laura </p>
+          </div>
             <section id="info-cliente">
-                <article class="col-lg-5">
+                <article class="col-md-5">
 
-                  <div class ="perfil">
-                    <img class="foto-perfil" src="/imgs/login-foto.png">
-                    <p class = "small-title-black">Diana Laura Martínez García</p>
-                    <button class="boton-azul"> Editar </button>
+                <div class = "profile-client">
+                    <div class = "row">
+                      <section class = "profile">
+                        <article class = "col-md-5">
+                        <div class = "push-left">
+                          <img class="img-circle" src="/imgs/Sunny.jpg">
+                          </div>
+                        </article>
+
+                        <article class = "col-md-7">
+                          <div class = "spacer-laura2">
+                            <p class = "small-title-black">Diana Laura Martínez García</p>
+                          </div>
+                          <button class="boton-verde-laura"> Editar perfil </button>
+                        </article>
+
+                      </section>
+                    </div>
+
+                </div>
+
+                  <div class = "info-auto">
+                    <div class="row">
+                      <section class = "auto-specs">
+                        <article class="col-md-6">
+                          <div class = "spacer-laura">
+                          <p class = "texto-general-gris"> Marca </p>
+                          <p class = "texto-general-azul"> Audi </p>
+                          </div>
+
+                          <p class = "texto-general-gris"> Estado </p>
+                          <p class = "texto-general-azul"> En espera</p>
+                        </article>
+
+                        <article class="col-md-6">
+                          <div class = "spacer-laura">
+                          <p class = "texto-general-gris"> Modelo </p>
+                          <p class = "texto-general-azul"> A1 Sportback</p>
+                          </div>
+
+                          <p class = "texto-general-gris"> Costo </p>
+                          <p class = "texto-general-verde"> 340,000 MXN</p>
+                        </article>
+                      </section>
+                    </div>
                   </div>
-                  <div class = "caja perfil">
-                    <p class = "texto-general-gris"> Marca </p>
-                    <p class = "texto-general-azul"> Audi </p>
 
-                    <p class = "texto-general-gris"> Estado </p>
-                    <p class = "texto-general-azul"> En espera</p>
-
-                    <p class = "texto-general-gris"> Modelo </p>
-                    <p class = "texto-general-azul"> A1 Sportback</p>
-
-                    <p class = "texto-general-gris"> Modelo </p>
-                    <p class = "texto-general-azul"> A1 Sportback</p>
-
-                  </div>
                 </article>
             </section>
-            <section class="mensajes">
-                <article class="col-lg-7">
-                    <p> nada <p>
-                </article>
+
+            <section class="muro-cliente">
+              <article class="col-md-7">
+                <div class = "mensajes">
+                  <p class = "small-title-black">Mensajes</p>
+                  <div class = "texto-cliente">
+                    <textarea>¿Tienes alguna duda? </textarea>
+                  </div>
+
+                </div>
+              </article>
             </section>
+
+
+
         </div>
     </div>
 </section>
