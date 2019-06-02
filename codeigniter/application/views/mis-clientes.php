35b4faf621f02625eeb9393f2cebf4d9d2235484
Priscila Esquer <priscilaesquer@Priscilas-MBP.localdomain>
2019-06-02 09:39:32 -0700

    priscila es chila

---
+++ codeigniter/application/views/mis-clientes.php
@@ -0,0 +1,71 @@
+<section id="login">
+    <div class="container">
+        <p class="big-titles"> Hola, Saul </p>
+        <div class="row"> 
+            <div class="col-md-3">
+                <div class="cliente-box">
+                <div class="space-15"></div>
+                <center><img class="img-circle" src="/imgs/Sunny.jpg"></center>
+                <div class="space-15"></div>
+                <center><p class="texto-general-gris-bold"> Diana Laura Martinez Garcia </p></center>
+                <div class="space-15"></div>
+                <center><button class="boton-azul-db"> Chat </button>
+                <button class="boton-verde-db"> Editar </button></center>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="cliente-box">
+                <div class="space-15"></div>
+                <center><img class="img-circle" src="/imgs/Sunny.jpg"></center>
+                <div class="space-15"></div>
+                <center><p class="texto-general-gris-bold"> Diana Laura Martinez Garcia </p></center>
+                <div class="space-15"></div>
+                <center><button class="boton-azul-db"> Chat </button>
+                <button class="boton-verde-db"> Editar </button></center>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="cliente-box">
+                <div class="space-15"></div>
+                <center><img class="img-circle" src="/imgs/Sunny.jpg"></center>
+                <div class="space-15"></div>
+                <center><p class="texto-general-gris-bold"> Diana Laura Martinez Garcia </p></center>
+                <div class="space-15"></div>
+                <center><button class="boton-azul-db"> Chat </button>
+                <button class="boton-verde-db"> Editar </button></center>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="cliente-box">
+                <div class="space-15"></div>
+                <center><img class="img-circle" src="/imgs/Sunny.jpg"></center>
+                <div class="space-15"></div>
+                <center><p class="texto-general-gris-bold"> Diana Laura Martinez Garcia </p></center>
+                <div class="space-15"></div>
+                <center><button class="boton-azul-db"> Chat </button>
+                <button class="boton-verde-db"> Editar </button></center>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row"> 
+            <div class="col-md-3">
+                <div class="cliente-box">
+                <div class="space-15"></div>
+                <center><img class="img-circle" src="/imgs/Sunny.jpg"></center>
+                <div class="space-15"></div>
+                <center><p class="texto-general-gris-bold"> Diana Laura Martinez Garcia </p></center>
+                <div class="space-15"></div>
+                <center><button class="boton-azul-db"> Chat </button>
+                <button class="boton-verde-db"> Editar </button></center>
+                </div>
+            </div>
+
+</div>
+
+    </div>
+</section>
